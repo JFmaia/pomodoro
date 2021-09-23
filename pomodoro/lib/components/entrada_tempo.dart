2ee15704a2c70f0e018d44f46fa0ed698cd5e7c2
JFmaia <jfmaia741@gmail.com>
2021-09-23 10:08:47 -0300

    Modified File
    Criando layout da entrada de dados.

--- pomodoro/lib/components/entrada_tempo.dart
+++ pomodoro/lib/components/entrada_tempo.dart
@@ -12,6 +12,54 @@ class EntradaTempo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          this.title,
+          style: TextStyle(
+            fontSize: 25,
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Icon(
+                Icons.arrow_downward,
+                color: Colors.white,
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(15),
+                primary: Colors.red,
+              ),
+              onPressed: () {},
+            ),
+            Text(
+              "${this.valor} min",
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            ElevatedButton(
+              child: Icon(
+                Icons.arrow_upward,
+                color: Colors.white,
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(15),
+                primary: Colors.red,
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
