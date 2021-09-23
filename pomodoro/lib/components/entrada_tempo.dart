b002da4dfbd9261f8ad9b3b597db6aef33facf40
JFmaia <jfmaia741@gmail.com>
2021-09-23 09:35:13 -0300

    New File
    Criando arquivo da entrada de tempo mostrada no app.

---
+++ pomodoro/lib/components/entrada_tempo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EntradaTempo extends StatelessWidget {
+  final int valor;
+  final String title;
+
+  const EntradaTempo({
+    Key? key,
+    required this.valor,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
