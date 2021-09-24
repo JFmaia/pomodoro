05d8de9044ede947a44eebec6301e2fc94a68fd4
JFmaia <jfmaia741@gmail.com>
2021-09-24 16:05:16 -0300

    Modified File
    Codificando o layout do cronometro.

--- pomodoro/lib/components/cronometro.dart
+++ pomodoro/lib/components/cronometro.dart
@@ -5,6 +5,28 @@ class Cronometro extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.red,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Hora de Trabalhar',
+            style: TextStyle(
+              fontSize: 40,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 20),
+          Text(
+            "25:00",
+            style: TextStyle(
+              fontSize: 120,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
