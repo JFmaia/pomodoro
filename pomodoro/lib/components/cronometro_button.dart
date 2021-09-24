117a37cca3095a61028afa43fd4fe6b5f9bfbb8e
JFmaia <jfmaia741@gmail.com>
2021-09-24 16:27:22 -0300

    Modified File
    Fazendo mudanças no layout do button.

--- pomodoro/lib/components/cronometro_button.dart
+++ pomodoro/lib/components/cronometro_button.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 class CronometroButton extends StatelessWidget {
-  const CronometroButton({Key? key}) : super(key: key);
+  final String text;
+  final IconData icone;
+
+  const CronometroButton({
+    Key? key,
+    required this.text,
+    required this.icone,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      child: child,
+      style: ElevatedButton.styleFrom(
+        primary: Colors.black,
+        padding: EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 20,
+        ),
+        textStyle: TextStyle(
+          fontSize: 25,
+        ),
+      ),
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(
+              this.icone,
+              size: 35,
+            ),
+          ),
+          Text(this.text),
+        ],
+      ),
       onPressed: () {},
     );
   }
