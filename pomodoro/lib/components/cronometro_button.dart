8df4972b14cef6e0f0076a3413d96acd68bac31c
JFmaia <jfmaia741@gmail.com>
2021-09-24 16:08:52 -0300

    New File
    Botão do cronometro.

---
+++ pomodoro/lib/components/cronometro_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CronometroButton extends StatelessWidget {
+  const CronometroButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: child,
+      onPressed: () {},
+    );
+  }
+}
